8305ea3a4321773f5905e6662df8107fe7f345e2
RobertLMcCrary <rlmccrary1210@gmail.com>
2024-12-15 11:45:33 -0500

    fixing auth bug

--- src/app/api/webhooks/clerk/route.js
+++ src/app/api/webhooks/clerk/route.js
@@ -6,28 +6,22 @@ import connectMongoDB from '../../../../../lib/mongodb';
 
 export async function POST(req) {
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
     const headerPayload = headers();
     const svix_id = headerPayload.get('svix-id');
     const svix_timestamp = headerPayload.get('svix-timestamp');
     const svix_signature = headerPayload.get('svix-signature');
-
     const payload = await req.json();
     const body = JSON.stringify(payload);
-
     const webhook = new Webhook(WEBHOOK_SECRET);
     const evt = webhook.verify(body, {
         'svix-id': svix_id,
         'svix-timestamp': svix_timestamp,
         'svix-signature': svix_signature,
     });
-
     const { id, email_addresses, ...attributes } = evt.data;
-
     const client = await MongoClient.connect(process.env.MONGO_URI);
     const db = client.db('PseudoAI');
     const Users = db.collection('Users');
-
     if (evt.type === 'user.created') {
         await Users.insertOne({
             clerkId: id,
@@ -45,19 +39,6 @@ export async function POST(req) {
             solvedProblems: [],
         });
     }
-
-    if (evt.type === 'user.updated') {
-        await Users.updateOne(
-            { clerkId: id },
-            {
-                $set: {
-                    imageUrl: evt.data.image_url,
-                    username: evt.data.username,
-                },
-            }
-        );
-    }
-
     await client.close();
     return new Response('Webhook received', { status: 200 });
 }
